12944c906221c20abc93d7a0ffd5b140dcdc21ec
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-27 19:49:57 +0100

    Update omni_gta4l.mk

--- omni_gta4l.mk
+++ omni_gta4l.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from gta4l device
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
 PRODUCT_DEVICE := gta4l
-PRODUCT_NAME := omni_gta4l
+PRODUCT_NAME := twrp_gta4l
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T505
 PRODUCT_MANUFACTURER := samsung
