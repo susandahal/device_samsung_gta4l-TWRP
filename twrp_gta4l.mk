cfb48baca3c9f93634f2f29f9c5210401e6c7966
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-30 21:49:40 +0100

    Update and rename omni_gta4l.mk to twrp_gta4l.mk

--- twrp_gta4l.mk
+++ twrp_gta4l.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/pb/config/common.mk)
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
 PRODUCT_DEVICE := gta4l
-PRODUCT_NAME := omni_gta4l
+PRODUCT_NAME := twrp_gta4l
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T505
 PRODUCT_MANUFACTURER := samsung
