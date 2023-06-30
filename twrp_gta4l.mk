7dfdf70512ae176afc58dd9c78058dd730e10f21
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-30 21:13:24 +0100

    Update twrp_gta4l.mk

--- twrp_gta4l.mk
+++ twrp_gta4l.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from gta4l device
 $(call inherit-product, device/samsung/gta4l/device.mk)
